858cd30bdb7a5826226d8de4c2f3b739d6c28863
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2021-01-24 20:15:45 +0100

    Update eslint

--- index.test.js
+++ index.test.js
@@ -1,17 +1,17 @@
 const { Timeout, Interval } = require(".");
-require('jest');
+require("jest");
 
 beforeEach(() => {
     jest.useFakeTimers();
 });
 
-describe('state tests', () => {
-    test('state is set to 0 after creation of timer', () => {
+describe("state tests", () => {
+    test("state is set to 0 after creation of timer", () => {
         const to = new Timeout(() => { }, 0);
         expect(to.state).toBe(0);
     });
 
-    test('state is set to 1 when timer has been started', () => {
+    test("state is set to 1 when timer has been started", () => {
         const waitTime = 1000;
         const callback = jest.fn();
         const to = new Timeout(callback, waitTime);
@@ -19,7 +19,7 @@ describe('state tests', () => {
         expect(to.state).toBe(1);
     });
 
-    test('state is set to 3 and callback is called after timer is completed', () => {
+    test("state is set to 3 and callback is called after timer is completed", () => {
         const waitTime = 1000;
         const callback = jest.fn();
         const to = new Timeout(callback, waitTime);
@@ -32,35 +32,35 @@ describe('state tests', () => {
     });
 });
 
-describe('time start tests', () => {
-    test('timeout is not started when autostart is false', () => {
+describe("time start tests", () => {
+    test("timeout is not started when autostart is false", () => {
         const to = new Timeout(() => { }, 0, false);
         expect(setTimeout).toHaveBeenCalledTimes(0);
     });
 
-    test('timeout is not started when autostart is not specified', () => {
+    test("timeout is not started when autostart is not specified", () => {
         const to = new Timeout(() => { }, 0);
         expect(setTimeout).toHaveBeenCalledTimes(0);
     });
 
-    test('timeout is started when autostart is true', () => {
+    test("timeout is started when autostart is true", () => {
         const waitTime = 1000;
         const to = new Timeout(() => { }, waitTime, true);
         expect(setTimeout).toHaveBeenCalledTimes(1);
         expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), waitTime);
     });
 
-    test('interval is not started when autostart is false', () => {
+    test("interval is not started when autostart is false", () => {
         const to = new Interval(() => to.stop(), 1, false);
         expect(setInterval).toHaveBeenCalledTimes(0);
     });
 
-    test('interval is not started when autostart is not specified', () => {
+    test("interval is not started when autostart is not specified", () => {
         const to = new Interval(() => to.stop(), 1);
         expect(setInterval).toHaveBeenCalledTimes(0);
     });
 
-    test('interval is started when autostart is true', () => {
+    test("interval is started when autostart is true", () => {
         const waitTime = 1000;
         const to = new Interval(() => to.stop(), waitTime, true);
         expect(setInterval).toHaveBeenCalledTimes(1);
